898fce0edf4057dd741acfe76896139f2b55cfc6
hktalent <18223385+hktalent@users.noreply.github.com>
2023-03-30 09:50:23 +0800

    fixed 为高分辨率划痕模式 2023-03-30

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 rm $1/.DS_Store
-python run.py --input_folder "$1" --output_folder $PWD/output --GPU -1 --with_scratch
+python run.py --input_folder "$1" --output_folder $PWD/output --GPU -1 --with_scratch --HR
 
